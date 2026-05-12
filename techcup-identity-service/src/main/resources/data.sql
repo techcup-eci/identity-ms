a691d10ed6331b701c1890ee58b5ecd9f5fc159b
Rafael_Moreno <rafael.moreno@giitic.com>
2026-05-11 20:38:39 -0500

    fix:Datos quemados en la bd para probar login

--- techcup-identity-service/src/main/resources/data.sql
+++ techcup-identity-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (email, password, active, role) VALUES (
     'admin@techcup.com',
-    '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy',
+    '$2a$10$slYQmyNdgTY18LGvgxPwHOSQKeoBYFpMDCqSoFMnEHFSGMxhEMnGi',
     true,
     'ADMIN'
 );
